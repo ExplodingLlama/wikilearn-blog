52c4bea9c42f0fc2bb94cb5fdcedca7614621d75
Monty <ratan14@gmail.com>
2020-07-20 15:39:21 +0530

    some error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, StaticRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, StaticRouter, Route} from "react-router-dom";
 import './App.css';
 import axios from 'axios'
 import moment from 'moment'
